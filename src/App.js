81728a735b543b31d55bca3edf8a94530ba45773
Rob Mastronicola <arejaycola@gmail.com>
2021-08-20 11:02:15 -0400

    Test and code context for scoop subtotal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import OrderEntry from './pages/entry/OrderEntry';
+import { OrderDetailsProvider } from './contexts/OrderDetails';
 import SummaryForm from './pages/summary/SummaryForm';
 
 function App() {
-  return (
-    <SummaryForm />
-  );
+	return (
+		<Container>
+			<OrderDetailsProvider>
+				<OrderEntry />
+				<SummaryForm />
+			</OrderDetailsProvider>
+		</Container>
+	);
 }
 
 export default App;
