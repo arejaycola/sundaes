1a1d08fd4072baf81bb5425480c4d9f47c139138
Rob Mastronicola <arejaycola@gmail.com>
2021-08-26 09:44:04 -0400

    Order button is disabled until a scoop is selected

--- src/pages/entry/OrderEntry.js
+++ src/pages/entry/OrderEntry.js
@@ -5,17 +5,22 @@ import Options from './Options';
 const OrderEntry = () => {
 	const [orderDetails, updateItemCount, reset] = useOrderDetails();
 	const { setOrderPhase } = useWorkflow();
+
+	const numOfScoops = orderDetails.scoops.size;
 	
 	useEffect(() => {
 		/* Reset the count */
 		reset();
 	}, []);
+	
 	return (
 		<>
 			<Options optionType="scoops" />
 			<Options optionType="toppings" />
 			<h2 data-testid="grand-total">Grand Total: {orderDetails.totals.grandTotal}</h2>
-			<button onClick={() => setOrderPhase('review')}>Order Sundae!</button>
+			<button disabled={numOfScoops > 0 ? null : 'disabled'} onClick={() => setOrderPhase('review')}>
+				Order Sundae!
+			</button>
 		</>
 	);
 };
